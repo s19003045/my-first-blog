1401399652e7b11234ce6e4c8da0b3d26a8df69f
Your Name <you@example.com>
2019-02-22 09:35:49 +0800

    Unpublish_list & Publish

--- blog/urls.py
+++ blog/urls.py
@@ -21,10 +21,11 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
+	path('drafts/', views.post_draft_list, name = 'post_draft_list'),
+	path('post/<pk>/publish/', views.post_publish, name='post_publish'),
 ]
 
-from django.urls import path 
-from . import views
+
 
 
 
