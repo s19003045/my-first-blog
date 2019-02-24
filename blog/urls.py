05743c96f0e53ae09efad6ed04121b53f085401a
Your Name <you@example.com>
2019-02-25 03:41:52 +0800

    add Comment model/form

--- blog/urls.py
+++ blog/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.urls import path 
+from django.urls import path
 from . import views
 
 urlpatterns = [
@@ -24,10 +24,8 @@ urlpatterns = [
 	path('drafts/', views.post_draft_list, name = 'post_draft_list'),
 	path('post/<pk>/publish/', views.post_publish, name='post_publish'),
 	path('post/<pk>/remove/', views.post_remove, name='post_remove'),
+	path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
 
 ]
-
-
-
-
-
